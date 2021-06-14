41e272ed9f1debd5479e81b496513b6420cac734
krishna <svkrishna.p@apssdc.in>
2021-06-14 15:50:17 +0530

    added

--- InputControllers_gist/app/src/main/java/gist/edu/inputcontrollers_gist/MainActivity.java
+++ InputControllers_gist/app/src/main/java/gist/edu/inputcontrollers_gist/MainActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     /*InputControls : are used design the UI of an
@@ -23,17 +31,67 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radio1,radio2;
     Switch aSwitch;
     LinearLayout linearLayout;
+    CheckBox checkBox1,checkBox2,checkBox3;
+    Spinner spinner;
+    List<String> weeks;
+    AutoCompleteTextView autoct;
+    List<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkBox1 = findViewById(R.id.check1);
+        checkBox2 = findViewById(R.id.check2);
+        checkBox3 = findViewById(R.id.check3);
         linearLayout = findViewById(R.id.linear1);
         aSwitch = findViewById(R.id.switch1);
         rg = findViewById(R.id.rg1);
         radio1 = findViewById(R.id.rb1);
         radio2 = findViewById(R.id.rb2);
         imageButton = findViewById(R.id.img1);
+        spinner = findViewById(R.id.spinner1);
+        weeks = new ArrayList<>();
+        weeks.add("Sunday");
+        weeks.add("Monday");
+        weeks.add("Tuesday");
+        weeks.add("WednesDay");
+        weeks.add("Thursday");
+        weeks.add("Friday");
+        weeks.add("saturday");
+        autoct = findViewById(R.id.act1);
+        names = new ArrayList<>();
+        names.add("krishna");
+        names.add("anusha");
+        names.add("pavan");
+        names.add("sreekanth");
+        names.add("sunil");
+        names.add("meghana");
+        names.add("eagle");
+        names.add("doctor");
+        names.add("baby");
+        names.add("murthy");
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item,names);
+        autoct.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item,weeks);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "You Selected: "+
+                        weeks.get(i),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -64,6 +122,22 @@ public class MainActivity extends AppCompatActivity {
             linearLayout.setBackgroundColor(Color.GREEN);
         }else{
             linearLayout.setBackgroundColor(Color.YELLOW);
+        }
+    }
+
+    public void selectbleItems(View view) {
+        StringBuffer result = new StringBuffer();
+        if(checkBox1.isChecked()){
+            result.append(checkBox1.getText());
+        }if(checkBox2.isChecked()){
+            result.append("\n"+checkBox2.getText());
+
+        }
+        if(checkBox3.isChecked()){
+            result.append("\n"+checkBox3.getText());
+        }
+        if(checkBox1.isChecked()||checkBox2.isChecked()||checkBox3.isChecked()){
+            Toast.makeText(this, ""+result, Toast.LENGTH_SHORT).show();
         }
     }
 }
